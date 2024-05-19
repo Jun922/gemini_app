96f82b1c629713ea8194475bb5ad3ee8dbdb3352
Jun922 <junjunchi620@gmail.com>
2024-05-19 23:01:53 +0900

    route

--- app/manage.py
+++ app/manage.py
@@ -1,6 +1,15 @@
+from flask import Flask
 from config import model
 from src import main
 
 
 if __name__ == "__main__":
     main(model)
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+app.run(debug=True)
