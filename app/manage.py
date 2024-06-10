f892ffba751be7dc8a6da4dc3aa82b9891981fee
Jun922 <junjunchi620@gmail.com>
2024-06-10 22:20:03 +0900

    excel path

--- app/manage.py
+++ app/manage.py
@@ -1,3 +1,4 @@
+import pathlib
 from flask import Flask, render_template, request
 from config import model
 from src import main
@@ -16,8 +17,10 @@ def index():
 @app.route("/index", methods=["post"])
 def post():
     file_name = request.form["upload_file"]
-    
-    for line in main(model, file_name):
+    file_path = "" + file_name
+    # need abs path
+
+    for line in main(model, file_path):
         buff.append(line)
     return render_template("index.html", buff=buff)
 
